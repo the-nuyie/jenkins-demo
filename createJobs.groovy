a154a913906003563cc7bced8c989fb7249e58b3
Tinnakorn Siritanyapong <the.nuyie@gmail.com>
2022-11-04 16:33:47 +0700

    Nuyie edited

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/the-nuyie/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
